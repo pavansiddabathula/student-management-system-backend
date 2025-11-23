73a2f842817f186d3d238e411ae7dc728cd97272
pavanSiddabathula <pavansiddabathula@gmail.com>
2025-11-23 21:51:47 +0530

    (Refactor): modified the Random Admin id generate class . implemetnt with pattern based generator not random id generrator (ADM001)

--- src/main/java/com/techcode/studentmgmt/utils/AdminIdGenerator.java
+++ src/main/java/com/techcode/studentmgmt/utils/AdminIdGenerator.java
@@ -1,21 +1,22 @@
 package com.techcode.studentmgmt.utils;
 
-import java.security.SecureRandom;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class AdminIdGenerator {
 
-    private static final String CHAR_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String PREFIX = "ADM";
+    private static final int DIGITS = 3;
 
-    public String generateAdminId() {
-        SecureRandom random = new SecureRandom();
-        StringBuilder id = new StringBuilder();
+    public String generateAdminId(String lastId) {
 
-        for (int i = 0; i < 6; i++) {
-            id.append(CHAR_SET.charAt(random.nextInt(CHAR_SET.length())));
+        if (lastId == null) {
+            return PREFIX + String.format("%0" + DIGITS + "d", 1);
         }
-        return id.toString();
+
+        int number = Integer.parseInt(lastId.substring(PREFIX.length()));
+        number++;
+
+        return PREFIX + String.format("%0" + DIGITS + "d", number);
     }
 }
